41cebfef1df0bf5076464b129d3468ba242360f2
Kristinn Guðnason <kristinnjg20@ru.is>
2020-09-17 17:59:24 +0000

    Beinagrind 1

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,1 +1,56 @@
-test
+# TileTraveller
+
+
+def add_or(res):
+    if res != '':
+        res += ' or '
+    return res
+
+
+def yct(directions):
+    result = ''
+    for ch in directions:
+        if ch == 'n':
+            result = '(N)orth'
+        if ch == 's':
+            result = add_or(result)
+            result += '(S)outh'
+        if ch == 'e':
+            result = add_or(result)
+            result += '(E)ast'
+        if ch == 'w':
+            result = add_or(result)
+            result += '(W)est'
+    return result + '.'
+
+
+def room_directions(tile):
+    result = ''
+    if tile == '1,1':
+        result = 'n'
+    elif tile == '1,2':
+        result = 'nse'
+    return result
+
+
+def room_description(tile):
+    print('You can travel: ' + yct(room_directions(tile)))
+
+
+def move_rooms(tile, direction):
+    pass
+
+
+def is_victory_condition(tile):
+    if tile == '3,1':
+        result = True
+    else:
+        result = False
+    return result
+
+
+tile = '1,1'
+while not is_victory_condition(tile):
+    room_description(tile)
+    direction = input("Direction: ")
+    move_rooms(tile, direction)
